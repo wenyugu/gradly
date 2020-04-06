f9e84a376c85ce92717fdd02032cdea87b0d6692
Daniel Gu <wenyugu@Daniels-MacBook-Pro.local>
2020-04-06 18:35:17 -0500

    constants

---
+++ app/src/constants/constant.js
@@ -0,0 +1,19 @@
+export const defaultEducation = {
+    school: '',
+    degree: '',
+    major: '',
+    gpa: '',
+    year: 1900,
+    courses: [{ num: '', name: '' }]
+}
+export const defaultEducations = [defaultEducation]
+
+export const defaultExperience = {
+    employer: '',
+    industry: '',
+    title: '',
+    salary: '',
+    type: '',
+    rating: 0,
+}
+export const defaultExperiences = [defaultExperience]
